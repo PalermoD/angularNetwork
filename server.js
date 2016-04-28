d8271dcd55c02925094b60a10e49ef9b34cad395
PalermoD <palermo.deschamps@gmail.com>
2016-04-28 11:41:22 -0400

    configuring server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+
+var app = express();
+
+app.listen('3000', function (){
+	console.log("Listening on localhost:3000 my Nigga");
+});
