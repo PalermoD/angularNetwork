1c7ada9c12f8782ba61819a0bc62a7613f769e2d
PalermoD <palermo.deschamps@gmail.com>
2016-04-28 12:19:54 -0400

    server and data directory

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ var bodyParser = require("body-parser");
 
 var app = express();
 
+app.get('/', function(req, res){
+	res.sendfile('index.html');
+});
+
 app.listen('3000', function (){
 	console.log("Listening on localhost:3000 my Nigga");
 });
